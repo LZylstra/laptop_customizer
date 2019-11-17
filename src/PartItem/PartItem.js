fe2d9416054ae4df2e8fc9bcbc838ce1872df00f
LZylstra <lindseyzylstra@gmail.com>
2019-11-16 19:34:29 -0800

    separated files into components

---
+++ src/PartItem/PartItem.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+import slugify from 'slugify'
+
+class ListItem extends Component {
+    render() {
+        const options = this.props.features[this.props.feature].map(item => {
+            const itemHash = slugify(JSON.stringify(item));
+
+            return (
+                <div key={itemHash} className="feature__item">
+                  <input
+                    type="radio"
+                    id={itemHash}
+                    className="feature__option"
+                    name={slugify(this.props.feature)}
+                    checked={item.name === this.props.state.selected[this.props.feature].name}
+                    onChange={e => this.props.handleUpdate(this.props.feature, item)}
+                  />
+                  <label htmlFor={itemHash} className="feature__label">
+                    {item.name} ({this.props.USCurrencyFormat.format(item.cost)})
+                  </label>
+                </div>
+              );
+
+            });
+            
+        return (
+            <div>
+                {options}
+            </div>
+        )
+    }
+}
+
+
+export default ListItem
